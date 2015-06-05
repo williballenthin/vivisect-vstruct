7bfb0f9923b9a2c777611a120056e9688b60dec5
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-06-05 10:54:09 -0400

    setup.py: add details to package description

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ from vstruct import __version__
 from setuptools import setup
 
 
-description = "vivisect vstruct."
-setup(name="vivisect-vstruct",
+description = "Unofficial packaged vivisect vstruct mirror."
+setup(name="vivisect-vstruct-wb",
       version=__version__,
       description=description,
       long_description=description,
-      license="Apache 2.0 License",
-      packages=["vstruct"])
+      url="https://github.com/williballenthin/vivisect-vstruct",
+      author="invisig0th, mirrored by Willi Ballenthin",
+      author_email="willi.ballenthin@gmail.com",
+      packages=["vstruct"],
+      classifiers=[
+          "Intended Audience :: Developers",
+          'Programming Language :: Python :: 2.7',
+      ])
