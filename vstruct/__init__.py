545bfb4583da5086ccdf7c829e054e94e99034f4
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-07-16 09:49:12 -0400

    prims: pull enum support into v_number

--- vstruct/__init__.py
+++ vstruct/__init__.py
@@ -428,11 +428,12 @@ class VStruct(vs_prims.v_base):
                 rstr = repr(field) #field.vsGetTypeName()
             except TypeError:
                 print(off, name, field)
-            if isinstance(field, vs_prims.enum_mixin):
-                rstr = '%s (0x%.8x)' % (str(field), field.vsGetValue())
-            elif isinstance(field, vs_prims.v_number):
-                val = field.vsGetValue()
-                rstr = '0x%.8x (%d)' % (val,val)
+            if isinstance(field, vs_prims.v_number):
+                if field.vsGetEnum() is not None:
+                    rstr = '%s (0x%.8x)' % (str(field), field.vsGetValue())
+                else:
+                    val = field.vsGetValue()
+                    rstr = '0x%.8x (%d)' % (val,val)
             elif isinstance(field, vs_prims.v_prim):
                 rstr = repr(field)
             if reprmax != None and len(rstr) > reprmax:
