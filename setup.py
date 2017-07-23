905f2f5802874a5e9c51432811a48267b015ee26
Willi Ballenthin <wilbal1087@gmail.com>
2017-07-23 08:50:40 -0700

    setup: use find_packages to correctly include nested modules

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
 
 description = "Unofficial packaged vivisect vstruct mirror."
-setup(name="vivisect-vstruct-wb",
+setuptools.setup(name="vivisect-vstruct-wb",
       version="1.0.3",
       description=description,
       long_description=description,
       url="https://github.com/williballenthin/vivisect-vstruct",
       author="invisig0th, mirrored by Willi Ballenthin",
       author_email="willi.ballenthin@gmail.com",
-      packages=["vstruct"],
+      packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       classifiers=[
           "Intended Audience :: Developers",
           'Programming Language :: Python :: 2.7',
